d905b054c5c52133a1aa4179d9c538ae2574e50e
vanikk54 <skugoli@gmail.com>
2025-04-13 19:57:45 +0300

    Update

--- Task03/util.h
+++ Task03/util.h
@@ -1,1 +1,12 @@
 #include "logic.h"
+
+#include <iostream>
+#include <string>
+
+#define BUF 100
+
+using namespace std;
+
+string convert(int matrix[BUF][BUF], int size);
+void init_random(int matrix[BUF][BUF], int size, int a, int b);
+void init_user(int matrix[BUF][BUF], int size);
